dfe0ec4a4d4e554ffc518dadae9569a9d41f6745
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-06-14 13:37:50 +0200

    Add `socket.ping()` and `socket.pong()`

--- test.js
+++ test.js
@@ -62,3 +62,33 @@ test('secure', async (t) => {
     client.end('hello')
   })
 })
+
+test('ping pong', async (t) => {
+  t.plan(4)
+
+  const server = new ws.Server({ port: 8080 })
+
+  server.on('connection', (ws) => {
+    ws
+      .on('ping', (data) => {
+        t.alike(data, Buffer.from('hello'), 'received ping')
+
+        ws.end()
+      })
+      .on('close', () => {
+        server.close(() => {
+          t.pass('server closed')
+        })
+      })
+  })
+
+  server.on('listening', () => {
+    t.pass('listening')
+
+    const client = new ws.Socket({ port: 8080 })
+
+    client
+      .on('pong', (data) => t.alike(data, Buffer.from('hello'), 'received pong'))
+      .on('open', () => client.ping('hello'))
+  })
+})
